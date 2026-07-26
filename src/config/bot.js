0472f754900146cc2d7f2ccb23e57b4b90d99b4d
rvforu <aejsagolili09@gmail.com>
2026-07-26 12:57:07 -0400

    Update bot activity name in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "xxx", // required by Discord API, not shown in the client
+        name: "hi uno", // required by Discord API, not shown in the client
         state: "",     // this is what people actually see
         type: 1,               // Custom
       },
